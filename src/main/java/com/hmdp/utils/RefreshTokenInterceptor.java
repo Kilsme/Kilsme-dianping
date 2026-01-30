3cf54d4b00841f5032b5e36179a453747856c6b7
Kilsme <15868839+kilsme@user.noreply.gitee.com>
2026-01-31 01:32:35 +0800

    第一次初始化

---
+++ src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -0,0 +1,45 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+    private StringRedisTemplate stringRedisTemplate;
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate){
+        this.stringRedisTemplate = stringRedisTemplate;
+    }//因为这个类不是spring进行管理的所以要用构造函数进行注入，然后mvcconfig进行构建mvcconfig是spring进行管理构建的所以在mvcconfig进行注解注入
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1,从请求头中进行获取token
+        String token = request.getHeader("authorization");
+        if(StrUtil.isBlank(token)){
+            return true;
+        }
+        //基于token获取redis中的用户
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(RedisConstants.LOGIN_USER_KEY + token);
+        if(userMap.isEmpty()){
+            return true;
+        }
+        //将map装成userDTO
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        //存放到threadlocal中
+        UserHolder.saveUser(userDTO);
+        //进行刷新有效期
+        stringRedisTemplate.expire(RedisConstants.LOGIN_USER_KEY + token,RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
